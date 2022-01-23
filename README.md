13c7d9aa69f85a6619f095941df9bf52ec8e055f
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-01-23 23:43:47 +0600

    Complate Design

---
+++ README.md
@@ -0,0 +1,37 @@
+![Screenshot](./images/readme/cover.png)
+
+<br />
+<h1 style="text-align:center; border: none;">Vpn Landing Page</h1>
+
+<br />
+
+<h3>Vpn Landing Page Desine</h3>
+
+<h2>Used Language:</h2>
+<ul style="list-style: number;">
+    <li style="font-weight: bold;">HTML5</li>
+    <li style="font-weight: bold;">CSS3</li>
+    <li style="font-weight: bold;">SCSS</li>
+    <li style="font-weight: bold;">Javascript</li>
+    <li style="font-weight: bold;">Jquery</li>
+</ul>
+
+<br />
+<h2>Included:</h2>
+<ul style="list-style: number;">
+    <li style="font-weight: bold;">Responsive For All Device</li>
+    <li style="font-weight: bold;">W3C Validate Code</li>
+    <li style="font-weight: bold;">Modern Javascript ES6</li>
+    <li style="font-weight: bold;">100% Fresh Code</li>
+    <li style="font-weight: bold;">100% Lag Free</li>
+</ul>
+
+<br />
+<a href="https://developer-faras.github.io/vpn-landing-page/" style="padding: 10px 20px; background: #fff; text-decoration: none; color: rgb(55, 99, 245);" >Visit Demo</a>
+
+
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    // Testimonials Slider
+
+    let sliderWraper = $("#slider");
+    let slider = sliderWraper.find(".owl-carousel");
+    let navDotContainer = sliderWraper.find("#nav-dot-container");
+    let navContainer = navDotContainer.find('.nav-container');
+    let dotContainer = navDotContainer.find('.dot-container');
+
+    slider.owlCarousel({
+        items: 3,
+        loop: true,
+        // autoplay: true,
+        autoplayTimeout: 4000,
+        nav: true,
+        dots: true,
+        navContainer: navContainer,
+        dotsContainer: dotContainer,
+        responsive: {
+            992: {
+                items: 3
+            },
+            768: {
+                items: 2
+            },
+            600: {
+                items: 1
+            },
+            550:{
+                items: 1
+            },
+            400:{
+                items: 1
+            },
+            300:{
+                items: 1
+            }
+        }
+    });
+
+    // Footer List Title Click Disable
+    $("#main-footer").find(".list-title").click(function(e){
+        e.preventDefault();
+    });
+
+    // Nav Toggler
+    const navToggler = $("#mobile-menu-toggler");
+    const mobileMenu = $("#mobile-menu");
+
+    $(navToggler).click(function(){
+        $(mobileMenu).toggleClass('show');
+        $(this).toggleClass('fa-bars fa-times');
+    });
+
+    // Header Active On Scroll
+    const mainHeader = $("#main-header");
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 70){
+            mainHeader.addClass("active");
+        }else{
+            mainHeader.removeClass("active");
+        }
+    });
+});
+
+// Jquery Code
